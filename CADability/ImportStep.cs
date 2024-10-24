twobrainsgmbh/CADability

commit 38d08708693f2759eef162bec1abe6f702338bb1

Fixed the import of the step file "AVE_041_V9_ASRE.stp". This step file has nurbs faces with 2d edges, when the surface will be normalized, the edges must be also modified.

CADability/ImportStep.cs
@@ -411,6 +411,7 @@ VERTEX_POINT: C:\Zeichnungen\STEP\Ligna - Staab - Halle 1.stp (85207)
         private List<Item> notImportedFaces;
         private Dictionary<string, ColorDef> definedColors;
         private System.Collections.Concurrent.ConcurrentDictionary<string, IGeoObject> namedGeoObjects = new System.Collections.Concurrent.ConcurrentDictionary<string, IGeoObject>();
+        private System.Collections.Concurrent.ConcurrentDictionary<NurbsSurface, ModOp2D> nurbsSurfacesWithModifiedKnots; // when in construction the nurbs surface get a different 2d space, the modification is saved here
         class Tokenizer : IDisposable
         {
             StreamReader sr;
@@ -1231,6 +1232,7 @@ VERTEX_POINT: C:\Zeichnungen\STEP\Ligna - Staab - Halle 1.stp (85207)
             definitions = new List<Item>();
             importProblems = new Dictionary<int, string>();
             notImportedFaces = new List<Item>();
+            nurbsSurfacesWithModifiedKnots = new System.Collections.Concurrent.ConcurrentDictionary<NurbsSurface, ModOp2D>();
 #if DEBUG
             allNames = new SortedDictionary<string, int>();
             entityPattern = new Dictionary<string, HashSet<string>>();
@@ -2387,7 +2389,7 @@ VERTEX_POINT: C:\Zeichnungen\STEP\Ligna - Staab - Halle 1.stp (85207)
                                     pnt.Symbol = PointSymbol.Cross;
                                     pnt.Name = subItem.parameter["name"].sval;
                                     val.Add(pnt);
-									subItem.val = pnt; //For this CartesianPoint a Cadability Point is generated. I set it to the item instead of GeoPoint to make it work layer assignment.
+                                    subItem.val = pnt; //For this CartesianPoint a Cadability Point is generated. I set it to the item instead of GeoPoint to make it work layer assignment.
                                 }
                                 else if (o is IGeoObject) val.Add(o as IGeoObject);
                                 else if (o is GeoObjectList) val.AddRange(o as GeoObjectList);
@@ -4236,6 +4238,11 @@ VERTEX_POINT: C:\Zeichnungen\STEP\Ligna - Staab - Halle 1.stp (85207)
                                         }
                                     }
                                 }
+                                if (nurbsSurfacesWithModifiedKnots.TryGetValue(srf as NurbsSurface, out ModOp2D modify2DCurve))
+                                {
+                                    // this nurbs surface doesn't have the original 2d space. Move the 2d curve to the modified 2d space here.
+                                    c2d = c2d.GetModified(modify2DCurve);
+                                }
                                 if (item.val != null) item.val = srf.Make3dCurve(c2d);
                             }
                             else if (o is ICurve c3d)
@@ -4943,6 +4950,7 @@ VERTEX_POINT: C:\Zeichnungen\STEP\Ligna - Staab - Halle 1.stp (85207)
             // some NURBS surfaces come with extreme small uKnots or vKnots span. For better numerical behavior we normalize it here
             // this doesn't change the surface but only the 2d parameter space of the surface
             // an other case were we need to modify the knots span is with periodic surfaces that do not start with 0 as the first knot.
+            ModOp2D knotModification = ModOp2D.Identity;
             if (uKnots[uKnots.Length - 1] - uKnots[0] < 0.5 || (uClosed && uKnots[0] != 0.0))
             {
                 double f = 1.0 / (uKnots[uKnots.Length - 1] - uKnots[0]);
@@ -4951,6 +4959,8 @@ VERTEX_POINT: C:\Zeichnungen\STEP\Ligna - Staab - Halle 1.stp (85207)
                 {
                     uKnots[i] = (uKnots[i] - u0) * f;
                 }
+                knotModification[0, 0] = f;
+                knotModification[0, 2] = -u0;
             }
             if (vKnots[vKnots.Length - 1] - vKnots[0] < 0.5 || (vClosed && vKnots[0] != 0.0))
             {
@@ -4960,8 +4970,14 @@ VERTEX_POINT: C:\Zeichnungen\STEP\Ligna - Staab - Halle 1.stp (85207)
                 {
                     vKnots[i] = (vKnots[i] - v0) * f;
                 }
+                knotModification[1, 1] = f;
+                knotModification[1, 2] = -v0;
             }
             NurbsSurface res = new NurbsSurface(poles, weights, uKnots, vKnots, uMults, vMults, uDegree, vDegree, false, false);
+            if (!knotModification.IsIdentity)
+            {
+                nurbsSurfacesWithModifiedKnots[res] = knotModification; // remember the modification for later use
+            }
             // we should not make canonical surfaces here, because a nurbs surface in a "GEOMETRIC_SET" used in a "GEOMETRICALLY_BOUNDED_SURFACE_SHAPE_REPRESENTATION"
             // uses nurbs surfaces as faces with natural bounds
             switch (form)

commit 69d075fdfe516cc06fea5f6965d5e6c555f23be9

Fixed crash in step import introduced with rev 38d08708693f2759eef162bec1abe6f702338bb1 nurbsSurfacesWithModifiedKnots.TryGetValue the key can't be null.

CADability/ImportStep.cs
@@ -4238,7 +4238,7 @@ VERTEX_POINT: C:\Zeichnungen\STEP\Ligna - Staab - Halle 1.stp (85207)
                                         }
                                     }
                                 }
-                                if (nurbsSurfacesWithModifiedKnots.TryGetValue(srf as NurbsSurface, out ModOp2D modify2DCurve))
+                                if (srf is NurbsSurface nurbsSurface && nurbsSurfacesWithModifiedKnots.TryGetValue(nurbsSurface, out ModOp2D modify2DCurve))
                                 {
                                     // this nurbs surface doesn't have the original 2d space. Move the 2d curve to the modified 2d space here.
                                     c2d = c2d.GetModified(modify2DCurve);